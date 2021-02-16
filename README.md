476eec243358f68ce36d139f008eb710ed040cc0
Milvintsiss <Milvintsiss@users.noreply.github.com>
2021-02-16 20:35:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# SQLite3 library for windows
+
+This package help you bundle SQLite3 library to your apps.
+
+He was originally developed to use with moor but you can use it for others use cases that need SQLite3.
+
+## How to use with Moor
+
+Be sure to follow all the steps to migrate from moor_flutter to moor ffi ([doc](https://moor.simonbinder.eu/docs/other-engines/vm/)).
+
+Open an override for windows:
+
+    import 'dart:ffi';
+    import 'dart:io';
+    import 'package:sqlite3/sqlite3.dart';
+    import 'package:sqlite3/open.dart';
+	import 'package:sqlite3_library_windows/sqlite3_library_windows.dart';
+    
+    void main() {
+      open.overrideFor(OperatingSystem.windows, openOnWindows);
+    
+      final db = sqlite3.openInMemory();
+      db.dispose();
+	  
+	  runApp(MyApp());
+    }
+
+And... that's it! No need to provide you with your own sqlite3.dll file🙂
+
---
+++ lib/sqlite3_library_windows.dart
@@ -0,0 +1,43 @@
+library sqlite3_library_windows;
+
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+const sqlite3_windows_debug_libraryPath =
+    'build\\flutter_assets\\packages\\sqlite3_library_windows\\sqlite3.dll';
+const sqlite3_windows_release_libraryPath =
+    'data\\flutter_assets\\packages\\sqlite3_library_windows\\sqlite3.dll';
+
+///This function open SQLite3 in memory and return the associated DynamicLibrary
+///object.
+DynamicLibrary openSQLiteOnWindows() {
+  final libraryFile = File(kDebugMode
+      ? sqlite3_windows_debug_libraryPath
+      : sqlite3_windows_release_libraryPath);
+  print('SQLLite3 library PATH: ${libraryFile.absolute.path}');
+
+  DynamicLibrary library;
+  try {
+    library = DynamicLibrary.open(libraryFile.path);
+
+    print(_yellow("sqlite3 successfully loaded"));
+  } catch (e) {
+    try {
+      print(_red("Fail loading sqlite3 from library file, "
+          "trying loading from system..."));
+
+      library = DynamicLibrary.open('sqlite3.dll');
+
+      print(_yellow("sqlite3 successfully loaded"));
+    } catch (e) {
+      print(_red("Fail."));
+    }
+  }
+  return library;
+}
+
+String _red(String string) => '\x1B[31m$string\x1B[0m';
+
+String _yellow(String string) => '\x1B[32m$string\x1B[0m';
